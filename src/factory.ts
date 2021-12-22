ae10465ab06dcfcd6837c0702c0b5bfaf6d31e10
bonustrack <fabien@bonustrack.co>
2021-12-23 04:23:19 +0700

    Store Gnosis Safe SignMsg events

---
+++ src/factory.ts
@@ -0,0 +1,19 @@
+import { ProxyCreation as ProxyCreation } from '../generated/GnosisSafeProxyFactory/GnosisSafeProxyFactory'
+import { GnosisSafe, SignMsg } from '../generated/templates/GnosisSafe/GnosisSafe'
+import { GnosisSafe as GnosisSafeContract } from '../generated/templates'
+import { Sig } from '../generated/schema'
+
+export function handleProxyCreation(event: ProxyCreation): void {
+	let proxyAddress = event.params.proxy
+	let safeInstance = GnosisSafe.bind(proxyAddress)
+	let callGetOwnerResult = safeInstance.try_getOwners()
+	if (!callGetOwnerResult.reverted) {
+		GnosisSafeContract.create(proxyAddress)
+	}
+}
+
+export function handleSignMsg(event: SignMsg): void {
+	let sig = new Sig(event.transaction.hash.toHexString())
+	sig.msgHash = event.params.msgHash.toString()
+	sig.save()
+}
