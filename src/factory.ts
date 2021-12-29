e3fd4d1f6b392973176a9e22423145dd34ce98fa
bonustrack <fabien@bonustrack.co>
2021-12-29 13:03:04 +0700

    Add timestamp

--- src/factory.ts
+++ src/factory.ts
@@ -7,14 +7,13 @@ export function handleProxyCreation(event: ProxyCreation): void {
   let proxyAddress = event.params.proxy
   let safeInstance = GnosisSafe.bind(proxyAddress)
   let callGetOwnerResult = safeInstance.try_getOwners()
-  if (!callGetOwnerResult.reverted) {
-    GnosisSafeContract.create(proxyAddress)
-  }
+  if (!callGetOwnerResult.reverted) GnosisSafeContract.create(proxyAddress)
 }
 
 export function handleSignMsg(event: SignMsg): void {
   let sig = new Sig(event.transaction.hash.toHexString())
-  sig.safe = event.address
-  sig.msgHash = event.params.msgHash
+  sig.account = event.address
+  sig.msgHash = event.params.msgHash.toHex()
+  sig.timestamp = event.block.timestamp
   sig.save()
 }
