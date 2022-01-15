d69c940497f65ea5eb54eec2ebb16bcb666fc8ed
bonustrack <fabien@bonustrack.co>
2022-01-15 19:49:04 +0700

    Add support for Safe factory v1.1.1

--- src/factory.ts
+++ src/factory.ts
@@ -1,4 +1,5 @@
 import { Address } from '@graphprotocol/graph-ts'
+import { ProxyCreation as ProxyCreation_v1_1_1 } from '../generated/GnosisSafeProxyFactory_v1_1_1/GnosisSafeProxyFactory'
 import { ProxyCreation as ProxyCreation_v1_3_0 } from '../generated/GnosisSafeProxyFactory_v1_3_0/GnosisSafeProxyFactory'
 import { GnosisSafe, SignMsg } from '../generated/templates/GnosisSafe/GnosisSafe'
 import { GnosisSafe as GnosisSafeContract } from '../generated/templates'
@@ -8,6 +9,10 @@ function handleProxyCreation(proxyAddress: Address): void {
   let safeInstance = GnosisSafe.bind(proxyAddress)
   let callGetOwnerResult = safeInstance.try_getOwners()
   if (!callGetOwnerResult.reverted) GnosisSafeContract.create(proxyAddress)
+}
+
+export function handleProxyCreation_1_1_1(event: ProxyCreation_v1_1_1): void {
+  handleProxyCreation(event.params.proxy)
 }
 
 export function handleProxyCreation_1_3_0(event: ProxyCreation_v1_3_0): void {
