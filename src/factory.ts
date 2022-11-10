0298a8e99067053abe61a24ff40af02d8851e0f2
Chaitanya <yourchaitu@gmail.com>
2022-11-10 19:32:02 +0530

    Proxy factory v1.0.0 support - mainnet (#11)

--- src/factory.ts
+++ src/factory.ts
@@ -1,4 +1,5 @@
 import { Address } from '@graphprotocol/graph-ts'
+import { ProxyCreation as ProxyCreation_v1_0_0 } from '../generated/GnosisSafeProxyFactory_v1_0_0/GnosisSafeProxyFactory'
 import { ProxyCreation as ProxyCreation_v1_1_1 } from '../generated/GnosisSafeProxyFactory_v1_1_1/GnosisSafeProxyFactory'
 import { ProxyCreation as ProxyCreation_v1_3_0 } from '../generated/GnosisSafeProxyFactory_v1_3_0/GnosisSafeProxyFactory'
 import { GnosisSafe, SignMsg } from '../generated/templates/GnosisSafe/GnosisSafe'
@@ -9,6 +10,10 @@ function handleProxyCreation(proxyAddress: Address): void {
   let safeInstance = GnosisSafe.bind(proxyAddress)
   let callGetOwnerResult = safeInstance.try_getOwners()
   if (!callGetOwnerResult.reverted) GnosisSafeContract.create(proxyAddress)
+}
+
+export function handleProxyCreation_1_0_0(event: ProxyCreation_v1_0_0): void {
+  handleProxyCreation(event.params.proxy)
 }
 
 export function handleProxyCreation_1_1_1(event: ProxyCreation_v1_1_1): void {
