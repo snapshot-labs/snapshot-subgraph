c3e862712649f08bf3702d3fc9a06d82f6128e67
MosqueraGuillem <mosqueraguillem@gmail.com>
2023-06-01 12:54:15 +0200

    Add delegate entity and update mappings

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,6 +1,6 @@
 import { store, ethereum } from '@graphprotocol/graph-ts'
 import { SetDelegate, ClearDelegate } from '../generated/DelegateRegistry/DelegateRegistry'
-import { Block, Delegation } from '../generated/schema'
+import { Block, Delegation, Delegate } from '../generated/schema'
 
 export function handleBlock(block: ethereum.Block): void {
   let id = block.hash.toHex()
@@ -14,27 +14,43 @@ export function handleSetDelegate(event: SetDelegate): void {
   let delegator = event.params.delegator
   let space = event.params.id
   let delegate = event.params.delegate
-  let id = delegator.toHex()
+  let delegationId = delegator.toHex()
     .concat('-')
     .concat(space.toString())
     .concat('-')
     .concat(delegate.toHex())
-  let delegation = new Delegation(id)
+  let delegation = new Delegation(delegationId)
   delegation.delegator = delegator
   delegation.space = space.toString()
   delegation.delegate = delegate
   delegation.timestamp = event.block.timestamp.toI32()
   delegation.save()
+  let delegateId = delegate.toHex()
+  let delegateEntity = Delegate.load(delegateId)
+  if (!delegateEntity) {
+    delegateEntity = new Delegate(delegateId)
+    delegateEntity.address = delegate
+    delegateEntity.createdAt = event.block.timestamp.toI32()
+    delegateEntity.delegatorsCount = 0
+  }
+  delegateEntity.delegatorsCount += 1
+  delegateEntity.save()
 }
 
 export function handleClearDelegate(event: ClearDelegate): void {
   let delegator = event.params.delegator
   let space = event.params.id
   let delegate = event.params.delegate
-  let id = delegator.toHex()
+  let delegationId = delegator.toHex()
     .concat('-')
     .concat(space.toString())
     .concat('-')
     .concat(delegate.toHex())
-  store.remove('Delegation', id);
+  store.remove('Delegation', delegationId)
+  let delegateId = delegate.toHex()
+  let delegateEntity = Delegate.load(delegateId);
+  if (delegateEntity) {
+    delegateEntity.delegatorsCount -= 1;
+    delegateEntity.save();
+  }
 }
